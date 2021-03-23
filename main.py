c888b920557a58d642d3de338821b32dfab475b3
Влад <71227334+Comnate@users.noreply.github.com>
2021-03-23 18:34:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,25 @@ import telebot
 import pyowm
 import os
 
-# Set Telebot token
-
+# Set Telebot token with os.environ
 TelebotToken = os.environ['TelebotToken']
 bot = telebot.TeleBot(TelebotToken)
 
-# Set OpenWeatherMap token, russian language
-
+# Set OpenWeatherMap token with os.environ and russian language
 OWMToken = os.environ['OWMToken']
 owm = pyowm.OWM(OWMToken)
 owm.set_language('ru')
 
 # Set start command
-
-@bot.message_handler(commands = ['start'])
+@bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Введите название города')
 
 # Weather command
-
-@bot.message_handler(content_types = ['text'])
+@bot.message_handler(content_types=['text'])
 def weather(message):
 
     # Check if the city even exists
-
     try:
         owm.weather_at_place(message.text)
     except pyowm.exceptions.api_response_error.NotFoundError:
@@ -33,25 +28,20 @@ def weather(message):
         return
 
     # Get weather and detailed status
-
     weather = owm.weather_at_place(message.text).get_weather()
     detailed_status = weather.get_detailed_status()
-
     res = 'В городе ' + message.text + ' сейчас ' + detailed_status + '\n'
 
     # Get temperature
-
     temp = weather.get_temperature('celsius')
-
     res += 'Средняя температура ' + str(temp['temp']) + '\n'
-
     if temp['temp_max'] != temp['temp']:
         res += 'Максимальная температура ' + str(temp['temp_max']) + '\n'
     if temp['temp_min'] != temp['temp']:
         res += 'Минимальная температур ' + str(temp['temp_min']) + '\n'
 
+    # Send results
     bot.send_message(message.chat.id, res)
 
-    return
-
+# Set nonstop polling
 bot.polling(none_stop=True)
