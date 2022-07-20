5b33faf67e50626a76743869e7e7ff81cb4c707e
Влад <71227334+Skrynia@users.noreply.github.com>
2022-07-20 13:40:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,50 +7,45 @@ import telebot
 import pyowm
 
 
-def main():
-    @bot.message_handler(commands=['start'])
-    def start(message):
-        '''
-        Reaction on the start command
-        '''
-        bot.send_message(message.chat.id, 'Введите название города')
+TelebotToken = os.environ['TelebotToken']
+bot = telebot.TeleBot(TelebotToken)
 
-    @bot.message_handler(content_types=['text'])
-    def show_weather(message):
-        '''
-        Shows weather
-        '''
-        try:
-            owm.weather_at_place(message.text)
-        except pyowm.exceptions.api_response_error.NotFoundError:
-            bot.send_message(message.chat.id, 'Такого города не существует (вообще)')
-            return
+OWMToken = os.environ['OWMToken']
+owm = pyowm.OWM(OWMToken)
+owm.set_language('ru')
 
-        weather = owm.weather_at_place(message.text).get_weather()
-        detailed_status = weather.get_detailed_status()
-        res = 'В городе ' + message.text + ' сейчас ' + detailed_status + '\n'
+@bot.message_handler(commands=['start'])
+def start(message):
+    '''
+    Reaction on the start command
+    '''
+    bot.send_message(message.chat.id, 'Введите название города')
 
-        temp = weather.get_temperature('celsius')
-        temp.pop('temp_kf')
-        temp = {elem: str(round(temp[elem] / 5, 1) * 5) for elem in temp}
-        res += 'Средняя температура: ' + temp['temp'] + '°C'
-        if temp['temp_max'] != temp['temp']:
-            res += '\n' + 'Максимальная температура: ' + \
-                temp['temp_max'] + '°C'
-        if temp['temp_min'] != temp['temp']:
-            res += '\n' + 'Минимальная температура: ' + temp['temp_min'] + '°C'
+@bot.message_handler(content_types=['text'])
+def show_weather(message):
+    '''
+    Shows weather
+    '''
+    try:
+        owm.weather_at_place(message.text)
+    except pyowm.exceptions.api_response_error.NotFoundError:
+        bot.send_message(message.chat.id, 'Такого города не существует')
+        return
 
-        bot.send_message(message.chat.id, res)
+    weather = owm.weather_at_place(message.text).get_weather()
+    detailed_status = weather.get_detailed_status()
+    res = 'В городе ' + message.text + ' сейчас ' + detailed_status + '\n'
 
-    TelebotToken = os.environ['TelebotToken']
-    bot = telebot.TeleBot(TelebotToken)
+    temp = weather.get_temperature('celsius')
+    temp.pop('temp_kf')
+    temp = {elem: str(round(temp[elem] / 5, 1) * 5) for elem in temp}
+    res += 'Средняя температура: ' + temp['temp'] + '°C'
+    if temp['temp_max'] != temp['temp']:
+        res += '\n' + 'Максимальная температура: ' + \
+            temp['temp_max'] + '°C'
+    if temp['temp_min'] != temp['temp']:
+        res += '\n' + 'Минимальная температура: ' + temp['temp_min'] + '°C'
 
-    OWMToken = os.environ['OWMToken']
-    owm = pyowm.OWM(OWMToken)
-    owm.set_language('ru')
+    bot.send_message(message.chat.id, res)
 
-    bot.polling(none_stop=True)
-
-
-if __name__ == '__main__':
-    main()
+bot.polling(none_stop=True)
